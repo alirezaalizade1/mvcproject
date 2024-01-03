324a4b09239e624f45ed23bc9e570fe009b066a0
Your Name <you@example.com>
2024-01-03 03:33:16 -0800

    intial

---
+++ WebApplication8/Controllers/UserContoroler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public UserController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string GetName()
+        {
+            return "alireza";
+        }
+        public string GetName1(string name)
+        {
+            return name;
+        }
+        public string GetName2(string name, string family, int age)
+        {
+            return "fulname : " + name + " " + family + "\n age : " + age;
+        }
+        public string GetName3(string name, string family, int age)
+        {
+            return $"fulname{name}{family}\nage : {age}";
+        }
+        private string MyPrivate()
+        {
+            return "private method";
+        }
+        public IActionResult GetName4()
+        {
+            return View();
+        }
+        public IActionResult GetName5(string name1)
+        {
+            return View("GetName5",name1);
+        }
+    }
+}
